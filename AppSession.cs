db82cac064ae680ee819e812952fef09bf35ead4
Dustu <35203971+qhl088@users.noreply.github.com>
2022-02-28 16:13:07 +1300

    Add files via upload

---
+++ AppSession.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class AppSession
+    {
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string CalculatorAppId = @"D:\InterviewApp\InterviewApp.exe";
+        protected static WindowsDriver<WindowsElement> session;
+        
+        private WindowsElement _InvildOpe;
+
+        
+
+        /// <summary>
+        /// 启动
+        /// </summary>
+        public static void SetUp()
+        {
+            if(session == null)
+            {
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app",CalculatorAppId);
+                appCapabilities.SetCapability("deviceName","WindosPC");
+                //"deviceName", "WindowsPC"
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl),appCapabilities);
+                Assert.IsNotNull(session);
+                session.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1.5));
+            }
+        }
+        
+        /// <summary>
+        /// 退出
+        /// </summary>
+        public static void TearDown()
+        {
+            if(session!=null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+
+        public bool IsWinBoxExist
+        {
+            get
+            {
+                _InvildOpe = session.FindElementById("2A.3C074A");
+                return _InvildOpe.Displayed;
+            }
+        }
+
+        //public void IsWarningExist()
+        //{
+        //    try
+        //    {
+        //        if(IsWinBoxExist==true)
+        //        {
+        //            switch()
+        //            {
+        //                case a:
+        //                    break;
+        //            }                                          
+        //        }
+        //    }
+        //    catch
+        //    {
+
+        //    }
+        
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1:AppSession
+    {
+        private static WindowsElement CalculationResult;
+        [TestMethod]
+        public void Addition()
+        {
+            session.FindElementByAccessibilityId("num1").SendKeys("9");
+            session.FindElementByAccessibilityId("ops").SendKeys("{+}");
+            //运算符"{加减乘除}"
+            session.FindElementByAccessibilityId("num2").SendKeys("9");
+            session.FindElementByAccessibilityId("button1").Click();
+            Assert.AreEqual("18", GetCalculatorResultText());
+        }
+
+        [TestMethod]
+        public void Subtraction()
+        {
+            session.FindElementByAccessibilityId("num1").SendKeys("9");
+            session.FindElementByAccessibilityId("ops").SendKeys("{-}");
+            //运算符"{加减乘除}"
+            session.FindElementByAccessibilityId("num2").SendKeys("9");
+            session.FindElementByAccessibilityId("button1").Click();
+            Assert.AreEqual("0", GetCalculatorResultText());
+        }
+
+        [TestMethod]
+        public void Multiplication()
+        {
+            session.FindElementByAccessibilityId("num1").SendKeys("9");
+            session.FindElementByAccessibilityId("ops").SendKeys("{*}");
+            //运算符"{加减乘除}"
+            session.FindElementByAccessibilityId("num2").SendKeys("9");
+            session.FindElementByAccessibilityId("button1").Click();
+            Assert.AreEqual("81", GetCalculatorResultText());
+        }
+
+        [TestMethod]
+        public void Division()
+        {
+            session.FindElementByAccessibilityId("num1").SendKeys("9");
+            session.FindElementByAccessibilityId("ops").SendKeys("{/}");
+            //运算符"{加减乘除}"
+            session.FindElementByAccessibilityId("num2").SendKeys("9");
+            session.FindElementByAccessibilityId("button1").Click();
+            Assert.AreEqual("1", GetCalculatorResultText());
+        }
+
+        [TestMethod]
+        [DataRow("1", "{+}", "7", "8")]
+        [DataRow("9", "{-}", "1", "8")]
+        [DataRow("8", "{/}", "8", "1")]
+        public void Templatized(string input1, string operation, string input2, string expectedResult)
+        {
+            // Run sequence of button presses specified above and validate the results
+            session.FindElementByAccessibilityId("num1").SendKeys(input1);
+            session.FindElementByAccessibilityId("ops").SendKeys(operation);
+            session.FindElementByAccessibilityId("num2").SendKeys(input2);
+            session.FindElementByAccessibilityId("button1").Click();
+            Assert.AreEqual(expectedResult, GetCalculatorResultText());
+        }
+
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Create session to launch a Calculator window
+            SetUp();
+
+            // Locate the calculatorResult element
+            //calculatorResult= session.FindElementByName("labResult");
+            CalculationResult = session.FindElementByAccessibilityId("answer");
+
+            Assert.AreEqual("", CalculationResult.Text);
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void Clear()
+        {
+            session.FindElementByAccessibilityId("num1").Clear();
+            session.FindElementByAccessibilityId("num2").Clear();
+            session.FindElementByAccessibilityId("ops").Clear();
+            //Assert.IsNull("0", GetCalculatorResultText());
+        }
+
+        private string GetCalculatorResultText()
+        {
+            return CalculationResult.Text.Trim();
+        }
+    }
+}
